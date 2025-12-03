d58d289184e5827b0e1861037aae137c08db9f66
Lisette Hernández <marian.hernandez@ingenieria.unam.edu>
2025-12-03 16:34:12 +0100

    chore(memo-me): hello world from Memo Me

---
+++ Memo Me/Memo Me/Memo_MeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memo_MeApp.swift
+//  Memo Me
+//
+//  Created by Marian Lisette Hernandez Guzman on 03/12/25.
+//
+
+import SwiftUI
+
+@main
+struct Memo_MeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
