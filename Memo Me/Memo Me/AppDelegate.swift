0133007ec0a39a1703d45bfe07db3535b02f65e2
Lisette Hernández <marian.hernandez@ingenieria.unam.edu>
2025-12-13 19:08:44 +0100

    chore: add firebase configuration files

---
+++ Memo Me/Memo Me/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  Memo Me
+//
+//  Created by Marian Lisette Hernandez Guzman on 13/12/25.
+//
+
+import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
